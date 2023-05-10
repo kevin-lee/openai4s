e798260016a0e220b075180ae6ca8c4115c212e0
Kevin Lee <kevin.code@kevinlee.io>
2023-05-10 20:18:36 +1000

    Close #37 - Add completions.Response

---
+++ modules/openai4s-core/shared/src/main/scala-2/openai4s/types/completions/Response.scala
@@ -0,0 +1,179 @@
+package openai4s.types.completions
+
+import cats.{Eq, Show}
+import eu.timepit.refined.cats.*
+import eu.timepit.refined.types.string.NonEmptyString
+import extras.render.Render
+import extras.render.refined.*
+import io.circe.generic.extras.Configuration
+import io.circe.refined.*
+import io.circe.{Codec, Decoder, Encoder}
+import io.estatico.newtype.macros.newtype
+
+import java.time.Instant
+
+/** https://platform.openai.com/docs/api-reference/completions/create#:~:text=%22%5Cn%22%0A%7D-,Response
+  * @author Kevin Lee
+  * @since 2023-05-01
+  */
+final case class Response(
+  id: Response.Id,
+  `object`: Response.Object,
+  created: Response.Created,
+  model: Model,
+  choices: List[Response.Choice],
+  usage: Response.Usage,
+)
+object Response {
+
+  implicit val responseConfiguration: Configuration = Configuration.default.withSnakeCaseMemberNames
+
+  implicit val responseEq: Eq[Response] = Eq.fromUniversalEquals
+
+  implicit val responseShow: Show[Response] = cats.derived.semiauto.show
+
+  implicit val responseEncoder: Encoder[Response] =
+    io.circe.generic.extras.semiauto.deriveConfiguredEncoder[Response].mapJson(_.deepDropNullValues)
+  implicit val responseDecoder: Decoder[Response] = io.circe.generic.extras.semiauto.deriveConfiguredDecoder
+
+  @newtype case class Id(value: NonEmptyString)
+  object Id {
+    implicit val idEq: Eq[Id] = deriving
+
+    implicit val idRender: Render[Id] = deriving
+    implicit val idShow: Show[Id]     = deriving
+
+    implicit val idEncoder: Encoder[Id] = deriving
+    implicit val idDecoder: Decoder[Id] = deriving
+  }
+
+  @newtype case class Object(value: NonEmptyString)
+  object Object {
+    implicit val objectEq: Eq[Object] = deriving
+
+    implicit val objectRender: Render[Object] = deriving
+    implicit val objectShow: Show[Object]     = deriving
+
+    implicit val objectEncoder: Encoder[Object] = deriving
+    implicit val objectDecoder: Decoder[Object] = deriving
+  }
+
+  @newtype case class Created(value: Instant)
+  object Created {
+    implicit val createdEq: Eq[Created] = Eq.fromUniversalEquals
+
+    implicit val createdRender: Render[Created] = Render.fromToString
+    implicit val createdShow: Show[Created]     = Show.fromToString
+
+    implicit val createdEncoder: Encoder[Created] = Encoder[Long].contramap(_.value.getEpochSecond)
+    implicit val createdDecoder: Decoder[Created] = Decoder[Long].map(Instant.ofEpochSecond).map(Created(_))
+  }
+
+  final case class Choice(
+    text: Choice.Text,
+    index: Choice.Index,
+    logprobs: Option[Choice.Logprobs],
+    finishReason: Choice.FinishReason,
+  )
+  object Choice {
+    implicit val choiceEq: Eq[Choice] = Eq.fromUniversalEquals
+
+    implicit val choiceShow: Show[Choice] = cats.derived.semiauto.show
+
+    implicit val choiceCodec: Codec[Choice] = io.circe.generic.extras.semiauto.deriveConfiguredCodec
+
+    @newtype case class Text(value: NonEmptyString)
+    object Text {
+      implicit val textEq: Eq[Text] = deriving
+
+      implicit val textShow: Show[Text]     = deriving
+      implicit val textRender: Render[Text] = deriving
+
+      implicit val textEncoder: Encoder[Text] = deriving
+      implicit val textDecoder: Decoder[Text] = deriving
+    }
+
+    @newtype case class Index(value: Int)
+    object Index {
+      implicit val indexEq: Eq[Index] = deriving
+
+      implicit val indexShow: Show[Index]     = deriving
+      implicit val indexRender: Render[Index] = deriving
+
+      implicit val indexEncoder: Encoder[Index] = deriving
+      implicit val indexDecoder: Decoder[Index] = deriving
+    }
+
+    @newtype case class Logprobs(value: Int)
+    object Logprobs {
+      implicit val logprobsEq: Eq[Logprobs] = deriving
+
+      implicit val logprobsShow: Show[Logprobs] = deriving
+
+      implicit val logprobsEncoder: Encoder[Logprobs] = deriving
+      implicit val logprobsDecoder: Decoder[Logprobs] = deriving
+    }
+
+    @newtype case class FinishReason(value: String)
+    object FinishReason {
+      implicit val finishReasonEq: Eq[FinishReason] = deriving
+
+      implicit val finishReasonShow: Show[FinishReason]     = deriving
+      implicit val finishReasonRender: Render[FinishReason] = deriving
+
+      implicit val finishReasonEncoder: Encoder[FinishReason] = deriving
+      implicit val finishReasonDecoder: Decoder[FinishReason] = deriving
+    }
+
+  }
+
+  final case class Usage(
+    promptTokens: Usage.PromptTokens,
+    completionTokens: Usage.CompletionTokens,
+    totalTokens: Usage.TotalTokens,
+  )
+  object Usage {
+    implicit val usageEq: Eq[Usage] = Eq.fromUniversalEquals
+
+    implicit val usageShow: Show[Usage] = cats.derived.semiauto.show
+
+    implicit val usageCodec: Codec[Usage] = io.circe.generic.extras.semiauto.deriveConfiguredCodec
+
+    @newtype case class PromptTokens(value: Int)
+
+    object PromptTokens {
+      implicit val promptTokensEq: Eq[PromptTokens] = deriving
+
+      implicit val promptTokensShow: Show[PromptTokens]     = deriving
+      implicit val promptTokensRender: Render[PromptTokens] = deriving
+
+      implicit val promptTokensEncoder: Encoder[PromptTokens] = deriving
+      implicit val promptTokensDecoder: Decoder[PromptTokens] = deriving
+    }
+
+    @newtype case class CompletionTokens(value: Int)
+
+    object CompletionTokens {
+      implicit val completionTokensEq: Eq[CompletionTokens] = deriving
+
+      implicit val completionTokensShow: Show[CompletionTokens]     = deriving
+      implicit val completionTokensRender: Render[CompletionTokens] = deriving
+
+      implicit val completionTokensEncoder: Encoder[CompletionTokens] = deriving
+      implicit val completionTokensDecoder: Decoder[CompletionTokens] = deriving
+    }
+
+    @newtype case class TotalTokens(value: Int)
+
+    object TotalTokens {
+      implicit val totalTokensEq: Eq[TotalTokens] = deriving
+
+      implicit val totalTokensShow: Show[TotalTokens]     = deriving
+      implicit val totalTokensRender: Render[TotalTokens] = deriving
+
+      implicit val totalTokensEncoder: Encoder[TotalTokens] = deriving
+      implicit val totalTokensDecoder: Decoder[TotalTokens] = deriving
+    }
+
+  }
+}
--- modules/openai4s-core/shared/src/test/scala/openai4s/types/completions/Gens.scala
+++ modules/openai4s-core/shared/src/test/scala/openai4s/types/completions/Gens.scala
@@ -1,7 +1,12 @@
 package openai4s.types.completions
 
+import cats.syntax.all.*
 import hedgehog.*
+import hedgehog.extra.NumGens
 import openai4s.compat.TypesCompat
+import hedgehog.cats.instances.*
+
+import java.time.Instant
 
 /** @author Kevin Lee
   * @since 2023-05-08
@@ -60,5 +65,86 @@ object Gens extends TypesCompat {
         logprobs    <- genLogprobs.option
         stop        <- genStop.option
       } yield Text(model, prompt, maxTokens, temperature, topP, n, stream, logprobs, stop)
+  }
+
+  object response {
+
+    @SuppressWarnings(Array("org.wartremover.warts.IterableOps"))
+    def genId: Gen[Response.Id] = {
+      Gen
+        .string(Gen.alphaNum, Range.linear(1, 20))
+        .map(NonEmptyString.unsafeFrom)
+        .list(Range.singleton(2))
+        .map(_.reduce(_ ++ NonEmptyString("-") ++ _))
+        .map(Response.Id(_))
+    }
+
+    def genObject: Gen[Response.Object] =
+      Gen
+        .string(Gen.choice1(Gen.alphaNum, Gen.constant('.')), Range.linear(1, 10))
+        .map(NonEmptyString.unsafeFrom)
+        .map(Response.Object(_))
+
+    def genCreated: Gen[Response.Created] =
+      for {
+        now        <- Gen.constant(Instant.now())
+        (min, max) <- NumGens.genLongMinMaxPair(0L, 10L * 60)
+        diff       <- Gen.long(Range.linear(min, max))
+        created = Instant.ofEpochSecond(now.minusSeconds(diff).getEpochSecond)
+      } yield Response.Created(created)
+
+    def genPromptTokens: Gen[Response.Usage.PromptTokens] =
+      Gen.int(Range.linear(1, 100)).map(Response.Usage.PromptTokens(_))
+
+    def genCompletionTokens: Gen[Response.Usage.CompletionTokens] =
+      Gen.int(Range.linear(1, 100)).map(Response.Usage.CompletionTokens(_))
+
+    def genUsage: Gen[Response.Usage] =
+      for {
+        promptTokens     <- genPromptTokens
+        completionTokens <- genCompletionTokens
+        totalTokens = Response.Usage.TotalTokens(promptTokens.value + completionTokens.value)
+
+      } yield Response.Usage(
+        promptTokens = promptTokens,
+        completionTokens = completionTokens,
+        totalTokens = totalTokens,
+      )
+
+    def genText: Gen[Response.Choice.Text] =
+      Gen
+        .string(Gen.unicode, Range.linear(1, 800))
+        .map(content => Response.Choice.Text(NonEmptyString.unsafeFrom(content)))
+
+    def genFinishReason: Gen[Response.Choice.FinishReason] =
+      Gen.string(Gen.alphaNum, Range.linear(3, 10)).map(Response.Choice.FinishReason(_))
+
+    def genLogprobs: Gen[Response.Choice.Logprobs] =
+      Gen.int(Range.linear(1, Int.MaxValue)).map(Response.Choice.Logprobs(_))
+
+    def genChoice(index: Response.Choice.Index): Gen[Response.Choice] =
+      for {
+        text         <- genText
+        logprobs     <- genLogprobs.option
+        finishReason <- genFinishReason
+      } yield Response.Choice(text, index, logprobs, finishReason)
+
+    def genResponse: Gen[Response] =
+      for {
+        id      <- genId
+        obj     <- genObject
+        created <- genCreated
+        model   <- Gens.genModel
+        usage   <- genUsage
+        choices <- Gen
+                     .int(Range.linear(1, 10))
+                     .flatMap(upto =>
+                       (1 to upto)
+                         .toList
+                         .traverse { index =>
+                           genChoice(Response.Choice.Index(index))
+                         }
+                     )
+      } yield Response(id = id, `object` = obj, created = created, model = model, usage = usage, choices = choices)
   }
 }
