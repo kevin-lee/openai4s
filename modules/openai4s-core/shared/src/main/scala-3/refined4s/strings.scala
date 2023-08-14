1670b882f9e927ccb039e8e5f046e68ed65d86c8
Kevin Lee <kevin.code@kevinlee.io>
2023-08-14 19:57:39 +1000

    Fix #54 - Compile-time validation of Uri.apply doesn't work in Scala 3

--- modules/openai4s-core/shared/src/main/scala-3/refined4s/strings.scala
+++ modules/openai4s-core/shared/src/main/scala-3/refined4s/strings.scala
@@ -1,7 +1,10 @@
 package refined4s
 
 import scala.annotation.targetName
+import scala.quoted.{Expr, Quotes}
 import scala.util.control.NonFatal
+import scala.compiletime.*
+import scala.quoted.*
 
 /** @author Kevin Lee
   * @since 2023-04-23
@@ -24,8 +27,12 @@ object strings {
   }
 
   type Uri = Uri.Type
-  object Uri extends Refined[String] {
-    override inline def invalidReason(a: String): String =
+  object Uri extends InlinedRefined[String] {
+
+    override def invalidReason(a: String): String =
+      "It has to be a URI but got [" + a + "]"
+
+    override inline def inlinedInvalidReason(inline a: String): String =
       "It has to be a URI but got [" + a + "]"
 
     override def predicate(a: String): Boolean =
@@ -36,6 +43,29 @@ object strings {
         case NonFatal(_) =>
           false
       }
+
+    override inline def inlinedPredicate(inline uri: String): Boolean = ${ validateUri('uri) }
+
+    private def validateUri(uriExpr: Expr[String])(using Quotes): Expr[Boolean] = {
+      import quotes.reflect.*
+      uriExpr.asTerm match {
+        case Inlined(_, _, Literal(StringConstant(uriStr))) =>
+          try {
+            new java.net.URI(uriStr)
+            Expr(true)
+          } catch {
+            case _: Throwable => Expr(false)
+          }
+        case _ =>
+          report.error(
+            """Uri must be a string literal.
+              |If it's unknown in compile-time, use `Uri.from` or `Uri.unsafeFrom` instead.
+              |(unsafeFrom is not recommended)""".stripMargin,
+            uriExpr,
+          )
+          Expr(false)
+      }
+    }
 
   }
 }
