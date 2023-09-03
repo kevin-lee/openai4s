a99829f9d15c5b59c40660521e94c96c3e5f3395
Kevin Lee <kevin.code@kevinlee.io>
2023-09-03 21:18:20 +1000

    Close #80 - Update available ChatGPT models - GPT 4 models with 32k tokens are not available yet

--- modules/openai4s-core/shared/src/main/scala-2/openai4s/types/chat/Model.scala
+++ modules/openai4s-core/shared/src/main/scala-2/openai4s/types/chat/Model.scala
@@ -16,32 +16,79 @@ import io.circe.{Codec, Decoder, Encoder}
 sealed abstract class Model(val value: NonEmptyString)
 object Model {
   case object Gpt_4 extends Model(NonEmptyString("gpt-4"))
-  case object Gpt_4_0314 extends Model(NonEmptyString("gpt-4-0314"))
   case object Gpt_4_32k extends Model(NonEmptyString("gpt-4-32k"))
+
+  case object Gpt_4_0613 extends Model(NonEmptyString("gpt-4-0613"))
+  case object Gpt_4_32k_0613 extends Model(NonEmptyString("gpt-4-32k-0613"))
+
+  case object Gpt_4_0314 extends Model(NonEmptyString("gpt-4-0314"))
   case object Gpt_4_32k_0314 extends Model(NonEmptyString("gpt-4-32k-0314"))
 
   case object Gpt_3_5_Turbo extends Model(NonEmptyString("gpt-3.5-turbo"))
+  case object Gpt_3_5_turbo_16k extends Model(NonEmptyString("gpt-3.5-turbo-16k"))
+
+  case object Gpt_3_5_turbo_0613 extends Model(NonEmptyString("gpt-3.5-turbo-0613"))
+  case object Gpt_3_5_turbo_16k_0613 extends Model(NonEmptyString("gpt-3.5-turbo-16k-0613"))
+
   case object Gpt_3_5_Turbo_0301 extends Model(NonEmptyString("gpt-3.5-turbo-0301"))
 
   final case class Unsupported(override val value: NonEmptyString) extends Model(value)
 
-  def gpt_4: Model          = Gpt_4
+  def gpt_4: Model     = Gpt_4
+  def gpt_4_32k: Model = Gpt_4_32k
+
+  def gpt_4_0613: Model     = Gpt_4_0613
+  def gpt_4_32k_0613: Model = Gpt_4_32k_0613
+
   def gpt_4_0314: Model     = Gpt_4_0314
-  def gpt_4_32k: Model      = Gpt_4_32k
   def gpt_4_32k_0314: Model = Gpt_4_32k_0314
 
-  def gpt_3_5_Turbo: Model      = Gpt_3_5_Turbo
+  def gpt_3_5_Turbo: Model     = Gpt_3_5_Turbo
+  def gpt_3_5_turbo_16k: Model = Gpt_3_5_turbo_16k
+
+  def gpt_3_5_turbo_0613: Model     = Gpt_3_5_turbo_0613
+  def gpt_3_5_turbo_16k_0613: Model = Gpt_3_5_turbo_16k_0613
+
   def gpt_3_5_Turbo_0301: Model = Gpt_3_5_Turbo_0301
 
   def unsupported(value: NonEmptyString): Model = Unsupported(value)
 
+  def supportedValues: List[Model] =
+    List(
+      Model.gpt_4,
+      Model.gpt_4_32k,
+      //
+      Model.gpt_4_0613,
+      Model.gpt_4_32k_0613,
+      //
+      Model.gpt_4_0314,
+      Model.gpt_4_32k_0314,
+      //
+      Model.gpt_3_5_Turbo,
+      Model.gpt_3_5_turbo_16k,
+      //
+      Model.gpt_3_5_turbo_0613,
+      Model.gpt_3_5_turbo_16k_0613,
+      //
+      Model.gpt_3_5_Turbo_0301,
+    )
+
   def fromString(model: String): Either[String, Model] = model match {
     case Gpt_4.value.value => gpt_4.asRight
-    case Gpt_4_0314.value.value => gpt_4_0314.asRight
     case Gpt_4_32k.value.value => gpt_4_32k.asRight
+
+    case Gpt_4_0613.value.value => gpt_4_0613.asRight
+    case Gpt_4_32k_0613.value.value => gpt_4_32k_0613.asRight
+
+    case Gpt_4_0314.value.value => gpt_4_0314.asRight
     case Gpt_4_32k_0314.value.value => gpt_4_32k_0314.asRight
 
     case Gpt_3_5_Turbo.value.value => gpt_3_5_Turbo.asRight
+    case Gpt_3_5_turbo_16k.value.value => gpt_3_5_turbo_16k.asRight
+
+    case Gpt_3_5_turbo_0613.value.value => gpt_3_5_turbo_0613.asRight
+    case Gpt_3_5_turbo_16k_0613.value.value => gpt_3_5_turbo_16k_0613.asRight
+
     case Gpt_3_5_Turbo_0301.value.value => gpt_3_5_Turbo_0301.asRight
 
     case _ => s"Unknown model: $model".asLeft
--- modules/openai4s-core/shared/src/test/scala/openai4s/types/chat/Gens.scala
+++ modules/openai4s-core/shared/src/test/scala/openai4s/types/chat/Gens.scala
@@ -16,10 +16,15 @@ object Gens extends TypesCompat {
   def genModel: Gen[Model] =
     Gen.element1(
       Model.gpt_4,
-      Model.gpt_4_0314,
       Model.gpt_4_32k,
+      Model.gpt_4_0613,
+      Model.gpt_4_32k_0613,
+      Model.gpt_4_0314,
       Model.gpt_4_32k_0314,
       Model.gpt_3_5_Turbo,
+      Model.gpt_3_5_turbo_16k,
+      Model.gpt_3_5_turbo_0613,
+      Model.gpt_3_5_turbo_16k_0613,
       Model.gpt_3_5_Turbo_0301,
     )
 
