3d91e10aaf4e17141b97dbc14a34bb433764e8a6
Kevin Lee <kevin.code@kevinlee.io>
2023-09-03 21:18:20 +1000

    Close #80 - Update available ChatGPT models - GPT 4 models with 32k tokens are not available yet

--- modules/openai4s-core/shared/src/main/scala-3/openai4s/types/chat/Model.scala
+++ modules/openai4s-core/shared/src/main/scala-3/openai4s/types/chat/Model.scala
@@ -2,12 +2,7 @@ package openai4s.types.chat
 
 import cats.syntax.all.*
 import cats.{Eq, Show}
-//import eu.timepit.refined.cats.*
 import refined4s.strings.NonEmptyString
-//import eu.timepit.refined.types.string.NonEmptyString
-//import eu.timepit.refined.*
-//import eu.timepit.refined.api.*
-//import eu.timepit.refined.api.Refined.*
 import extras.render.Render
 import io.circe.{Codec, Decoder, Encoder}
 
@@ -19,35 +14,63 @@ import io.circe.{Codec, Decoder, Encoder}
   */
 enum Model(val value: NonEmptyString) derives CanEqual {
   case Gpt_4 extends Model(NonEmptyString("gpt-4"))
-  case Gpt_4_0314 extends Model(NonEmptyString("gpt-4-0314"))
   case Gpt_4_32k extends Model(NonEmptyString("gpt-4-32k"))
+
+  case Gpt_4_0613 extends Model(NonEmptyString("gpt-4-0613"))
+  case Gpt_4_32k_0613 extends Model(NonEmptyString("gpt-4-32k-0613"))
+
+  case Gpt_4_0314 extends Model(NonEmptyString("gpt-4-0314"))
   case Gpt_4_32k_0314 extends Model(NonEmptyString("gpt-4-32k-0314"))
 
   case Gpt_3_5_Turbo extends Model(NonEmptyString("gpt-3.5-turbo"))
+  case Gpt_3_5_turbo_16k extends Model(NonEmptyString("gpt-3.5-turbo-16k"))
+
+  case Gpt_3_5_turbo_0613 extends Model(NonEmptyString("gpt-3.5-turbo-0613"))
+  case Gpt_3_5_turbo_16k_0613 extends Model(NonEmptyString("gpt-3.5-turbo-16k-0613"))
+
   case Gpt_3_5_Turbo_0301 extends Model(NonEmptyString("gpt-3.5-turbo-0301"))
 
   case Unsupported(override val value: NonEmptyString) extends Model(value)
 }
 object Model {
 
-  def gpt_4: Model          = Gpt_4
+  def gpt_4: Model     = Gpt_4
+  def gpt_4_32k: Model = Gpt_4_32k
+
+  def gpt_4_0613: Model     = Gpt_4_0613
+  def gpt_4_32k_0613: Model = Gpt_4_32k_0613
+
   def gpt_4_0314: Model     = Gpt_4_0314
-  def gpt_4_32k: Model      = Gpt_4_32k
   def gpt_4_32k_0314: Model = Gpt_4_32k_0314
 
-  def gpt_3_5_Turbo: Model      = Gpt_3_5_Turbo
+  def gpt_3_5_Turbo: Model     = Gpt_3_5_Turbo
+  def gpt_3_5_turbo_16k: Model = Gpt_3_5_turbo_16k
+
+  def gpt_3_5_turbo_0613: Model     = Gpt_3_5_turbo_0613
+  def gpt_3_5_turbo_16k_0613: Model = Gpt_3_5_turbo_16k_0613
+
   def gpt_3_5_Turbo_0301: Model = Gpt_3_5_Turbo_0301
 
   def unsupported(value: NonEmptyString): Model = Unsupported(value)
 
   def supportedValues: List[Model] =
     List(
-      Model.Gpt_4,
-      Model.Gpt_4_0314,
-      Model.Gpt_4_32k,
-      Model.Gpt_4_32k_0314,
-      Model.Gpt_3_5_Turbo,
-      Model.Gpt_3_5_Turbo_0301,
+      Model.gpt_4,
+      Model.gpt_4_32k,
+      //
+      Model.gpt_4_0613,
+      Model.gpt_4_32k_0613,
+      //
+      Model.gpt_4_0314,
+      Model.gpt_4_32k_0314,
+      //
+      Model.gpt_3_5_Turbo,
+      Model.gpt_3_5_turbo_16k,
+      //
+      Model.gpt_3_5_turbo_0613,
+      Model.gpt_3_5_turbo_16k_0613,
+      //
+      Model.gpt_3_5_Turbo_0301,
     )
 
   def fromString(model: String): Either[String, Model] =
